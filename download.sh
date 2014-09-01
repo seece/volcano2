9665eff2c6d10e4cd8f5116d5f85263cbba36bdb
Joona Rossi <joona.rossi@kapsi.fi>
2014-09-01 23:26:44 +0300

    Datafiles in lowercase.

--- download.sh
+++ download.sh
@@ -5,6 +5,14 @@ if [ ! -d "game" ]; then
     wget --referer="http://www.suomipelit.fi/pelit/20/Volcano+2" www.suomipelit.fi/pelit/lataa/26
     unzip 26
     rm 26
+    mv SOUNDS sounds
+    for i in * sounds/*
+    do
+        if [ "$i" != "sounds" -a "$i" != "file_id.diz" ]; then
+            mv $i $(echo $i|tr [A-Z] [a-z])
+        fi 
+    done
+    rm *.exe *.zip
     echo "Volcano2 downloaded."
 else
     echo "Volcano2 already downloaded."
